c03346e22e6fe56c3325cfd79289c028188a1f43
jasper <jasper.angl@rub.de>
2022-10-23 09:13:23 +0200

    A test commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+/* HelloWorld.java
+ */
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main
+{
+    // A test modification
+    public static void main(String[] args) {
+        // Here we will write all of our tests for now
+        System.out.println("Hello World!");
+        testRemoveByIndex();
+    }
+
+    public static void testRemoveByIndex() {
+        Student jason = new Student("Jason", "Bourne", 1, 80.4, new Date(2000, 6, 20));
+        Student jasonCopy = new Student("Jason", "Bourne", 1, 80.4, new Date(2000, 6, 20));
+        Student anna = new Student("Anna", "Bourne", 2, 72, new Date(2000, 9, 20));
+        Student peter = new Student("Peter", "Pahn", 3, 72.9, new Date(2000, 10, 20));
+        Student nina = new Student("Nina", "Andresen", 4, 80.4, new Date(2000, 6, 20));
+        Student dieter = new Student("Dieter", "Bohlen", 5, 80.4, new Date(2000, 6, 20));
+        Student tom = new Student("Tom", "Tewes", 6, 72, new Date(2000, 9, 20));
+        Student mark = new Student("Mark", "Tewes", 7, 72.9, new Date(2000, 10, 20));
+
+
+        StudentList studentList = new StudentList();
+        boolean s1 = studentList.add(jason);
+        boolean s2 = studentList.add(anna);
+        boolean s3 = studentList.add(peter);
+        boolean s1c = studentList.add(jasonCopy);
+        boolean s4 = studentList.add(nina);
+        boolean s5 = studentList.add(dieter);
+        boolean s6 = studentList.add(tom);
+        boolean s7 = studentList.add(mark);
+
+        StudentList stdListCopy = new StudentList(studentList);
+
+        System.out.println(studentList.equals(stdListCopy));
+        System.out.println(studentList.get(5));
+        System.out.println(stdListCopy.get(5));
+        stdListCopy.remove(5);
+        System.out.println(studentList.get(5));
+        System.out.println(stdListCopy.get(5) + "\n");
+        System.out.println(stdListCopy.toString());
+
+    }
+}
---
+++ src/StudentList.java
@@ -0,0 +1,149 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class StudentList {
+
+    private ArrayList<Student> studentList;
+
+    public StudentList() {
+        this.studentList = new ArrayList<Student>();
+    }
+
+    public StudentList(StudentList that)  {
+        /*
+             TODO: Since strings are immutable in Java, we need to convert all elements of each Student
+              to a String and parseit back to an object, only that way we avoid using libraries.
+         */
+        this.studentList = that.getStudentList();
+    }
+
+
+    /*
+        2.8 All StudentList Methods
+     */
+
+    // Helper methods, determining whether a student is already part of the studentList
+    public boolean containsStudent(Student student) {
+        for (Student s: studentList) {
+            if (s.equals(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *  Adds a student to the student list, if the student doesn't exist yet.
+     * @param student A Student object
+     * @return A boolean whether or not the student was added
+     */
+    public boolean add(Student student) {
+        if (this.containsStudent(student)) {
+            return false;
+        } else {
+            this.studentList.add(student);
+            return true;
+        }
+    }
+
+    /**
+     *  Removes a given student from the Student List
+     * @param student A student object
+     * @return True if given Student was removed and false the Student was not
+     *          in the list
+     */
+    public boolean remove(Student student) {
+        for(int i = 0; i < this.studentList.size(); i++) {
+            Student currStudent = this.studentList.get(i);
+            if (currStudent.equals(student)) {
+                this.studentList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Student remove(int pos) {
+        try {
+            return this.studentList.remove(pos);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    public Student get(int pos) {
+        try {
+            return this.studentList.get(pos);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns a list of positions of Students who's last name match the one from the input param.
+     * @param lastname
+     * @return a list of positions of Students who's last name match the one from the input param.
+     */
+    public ArrayList<Integer> findLastname(String lastname) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i < this.studentList.size(); i++) {
+            if (this.studentList.get(i).getLastname() == lastname) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Integer> findFirstname(String firstname) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i < this.studentList.size(); i++) {
+            if (this.studentList.get(i).getFirstname() == firstname) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public int findStudentId(long studentId) {
+        for (Student student : this.studentList) {
+            if (student.getStudentId() == studentId) {
+                return Long.valueOf(studentId).intValue();
+            }
+        }
+        return -1;
+    }
+
+    public int size() {
+        // You allowed this method ;)
+        return this.studentList.size();
+    }
+
+    private boolean containsId(final long studentId) {
+        for (Student student : this.studentList) {
+            if (student.getStudentId() == studentId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+        Get and Set Student List
+     */
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    @Override
+    public String toString() {
+        String finalString = "";
+        for (Student s : this.studentList) {
+            finalString += s.toString() + "\n";
+        }
+        return finalString;
+    }
+}
